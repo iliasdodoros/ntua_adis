7c06ac7e38904aa66fb8d2bfef47f37c89931702
ilias ntontoros <iliasdodoros@gmail.com>
2024-01-26 22:38:49 +0200

    dsqgen

--- queries/generate-queries.sh
+++ queries/generate-queries.sh
@@ -5,7 +5,7 @@ touch query.temp # dsqgen puts all the queries from the templates dir whose name
 for i in $(seq 1 1 99)
 do
 cat /home/user/ntua_adis/DSGen-software-code-3.2.0rc1/query_templates/query$i.tpl > query.temp # to create each query in a different file, each time write a new template name into the qlist file
-./dsqgen -INPUT query.temp -VERBOSE -SCALE 10 -DIALECT netezza -OUTPUT_DIR /home/user/ntua_adis/queries
+./dsqgen -DIRECTORY ../query_templates -INPUT query.temp -VERBOSE -SCALE 10 -DIALECT netezza -OUTPUT_DIR /home/user/ntua_adis/queries
 
 done
 rm query.temp

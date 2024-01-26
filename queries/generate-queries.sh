94534cacbaacef3f7e8d43c02d82ac7f9ec76b85
ilias ntontoros <iliasdodoros@gmail.com>
2024-01-26 22:59:39 +0200

    5

--- queries/generate-queries.sh
+++ queries/generate-queries.sh
@@ -1,17 +1,11 @@
-cd /home/user/ntua_adis/DSGen-software-code-3.2.0rc1/query_templates
-touch query.temp 
-for i in `ls query*tpl`
-do 
-    echo $i;  
-    echo "define _END = \"\";" >> $i
-done
 cd /home/user/ntua_adis/DSGen-software-code-3.2.0rc1/tools
 
 # dsqgen puts all the queries from the templates dir whose name are specified in the -INPUT (qlist) file in one output file
 for i in $(seq 1 1 99)
 do
 echo "query$i.tpl" > query.temp # to create each query in a different file, each time write a new template name into the qlist file
-./dsqgen -DIRECTORY /home/user/ntua_adis/DSGen-software-code-3.2.0rc1/query_templates -INPUT query$i.temp -VERBOSE -SCALE 1 -DIALECT netezza -OUTPUT_DIR /home/user/ntua_adis/queries
+./dsqgen -DIRECTORY ../query_templates -INPUT query$i.temp -VERBOSE -SCALE 10 -DIALECT netezza -OUTPUT_DIR /home/user/ntua_adis/queries
 
+mv ../../queries/query_0.sql ../../queries/"query$i.sql" 
 done
 rm query.temp
